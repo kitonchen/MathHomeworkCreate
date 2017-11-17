bae52a1075c606a967bade86a82d18a745889a4f
kiton <1026687121@qq.com>
2017-11-17 19:55:29 +0800

    添加模型代码，并适配控制器

---
+++ 数学题生成器/Model/calanswermodel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+<<<<<<< HEAD
+namespace homework_create
+{
+    public  class CalAnswermodel:ModelLinker
+=======
+namespace homwwork_create
+{
+    public  class CalAnswermodel
+>>>>>>> 2053cebe17d54cb9a055816498b5f23e28cb740c
+    {
+
+    }
+}
